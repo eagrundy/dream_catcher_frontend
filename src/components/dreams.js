7e7da316e90bc3d109ec9f7445c306dd53c91027
eagrundy <ericamalakian@gmail.com>
2021-02-26 10:13:22 -0800

    Updated dreams.js and index.html

--- src/components/dreams.js
+++ src/components/dreams.js
@@ -2,13 +2,17 @@ class Dreams {
     constructor() {
         this.dreams = []
         this.adapter = new DreamsAdapter()
-        // this.bindEventListeners()
+        this.initiBindingsAndEventListeners()
         this.fetchAndLoadDreams()
+    }
+
+    initiBindingsAndEventListeners() {
+        this.dreamsContainer = document.getElementById('dreams-container')
     }
 
     fetchAndLoadDreams() {
         this.adapter.getDreams().then(dreams => {
-            dreams.forEach(dream => this.dreams.push(dream))
+            dreams.forEach(dream => this.dreams.push(new Dream(dream)))
         })
         .then(() => {
             this.render()
@@ -16,7 +20,6 @@ class Dreams {
     }
 
     render() {
-        const dreamsContainer = document.getElementById('dreams-container')
-        dreamsContainer.innerHTML = 'My Dreams'
+        this.dreamsContainer.innerHTML = this.dreams.map(dream => dream.renderLi()).join(``)
     }
 }
