80f581dbd614d597eb617687fd595a2eae28da74
eagrundy <ericamalakian@gmail.com>
2021-02-27 11:10:13 -0800

    Fetch and create action

--- src/adapters/categoriesAdapter.js
+++ src/adapters/categoriesAdapter.js
@@ -1,14 +1,38 @@
-const BASE_URL = "http://localhost:3000"
-const CATEGORIES_URL = `${BASE_URL}/categories`
-const DREAMS_URL = `${BASE_URL}/dreams`
+// const BASE_URL = "http://localhost:3000"
+// const CATEGORIES_URL = `${BASE_URL}/categories`
+// const DREAMS_URL = `${BASE_URL}/dreams`
+
+// class CategoriesAdapter {
+//     constructor() {
+//         this.categoriesUrl = CATEGORIES_URL
+//         this.dreamsUrl = DREAMS_URL
+//     }
+
+//     getCategories() {
+//         return fetch(this.categoriesUrl).then(res => res.json())
+//     }
+// }
 
 class CategoriesAdapter {
     constructor() {
-        this.categoriesUrl = CATEGORIES_URL
-        this.dreamsUrl = DREAMS_URL
+        this.baseUrl = 'http://localhost:3000/categories'
     }
 
-    getDreams() {
+    getCategories() {
         return fetch(this.baseUrl).then(res => res.json())
+    }
+
+    createCategory(value) {
+        const category = {
+            name: value,
+        }
+        return fetch(this.baseUrl, {
+            method: 'POST',
+            headers: {
+                // 'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({ category }),
+        }).then(res => res.json())
     }
 }
