26de6d2cebbbc991b1a1a6cef21950a386d3d84c
eagrundy <ericamalakian@gmail.com>
2021-04-06 20:59:25 -0700

    Bulit dream constructor

--- src/dream.js
+++ src/dream.js
@@ -1,29 +1,32 @@
+
 class Dream {
-    constructor(dreamJSON) {
-        this.id = dreamJSON.id
-        this.name = dreamJSON.name
-        this.image_url = dreamJSON.image_url
-        this.description = dreamJSON.description
-        this.achieved = dreamJSON.achieved
-        this.date_achieved = dreamJSON.date_achieved
-        this.category_id = dreamJSON.category_id
-        this.category = dreamJSON.category
+    constructor({id, name, description, image_url, achieved, date_achieved, category_id}) {
+        this.name = name
+        this.description = description
+        this.image_url = image_url
+        this.achieved = achieved
+        this.date_achieved = date_achieved
+        this.category_id = category_id
+        
+        this.element = document.createElement("div")
+        this.element.id = `dream-${id}`
+        this.drinksList = document.querySelector("#dreams-list")
     }
 
-    renderLi() {
-        // return `<li>${this.name}
-        // ${this.description}</li>`
-        return `
-        <li>
-    <strong><h1>${this.name}</h1></strong>
-    <img src=${this.image_url} width="300" height="300" alt="">
-            <h3>${this.description}</h3><br>
-            <h3>Dream Achieved? ${this.achieved ? 'YES' : 'Not Yet'}</h3>
-            <h3>Date Achieved: ${this.date_achieved}</h3>
-            <h3>${this.category_id}</h3>
-        <button data-id=${this.id}>edit</button>
-      </h3>
-    </li><br>`;
-    }
+    // renderLi() {
+    //     // return `<li>${this.name}
+    //     // ${this.description}</li>`
+    //     return `
+    //     <li>
+    // <strong><h1>${this.name}</h1></strong>
+    // <img src=${this.image_url} width="300" height="300" alt="">
+    //         <h3>${this.description}</h3><br>
+    //         <h3>Dream Achieved? ${this.achieved ? 'YES' : 'Not Yet'}</h3>
+    //         <h3>Date Achieved: ${this.date_achieved}</h3>
+    //         <h3>${this.category_id}</h3>
+    //     <button data-id=${this.id}>edit</button>
+    //   </h3>
+    // </li><br>`;
+    // }
     
 }
