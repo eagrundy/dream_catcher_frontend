22fce096675e775b2480af232d50987efc6b8b86
eagrundy <ericamalakian@gmail.com>
2021-04-07 19:16:42 -0700

    Created DreamApi class

--- src/category.js
+++ src/category.js
@@ -1,11 +1,46 @@
+const baseURL = "http://localhost:3000/"
+const categoriesList = document.querySelector("#categories-list")
+const categoryShow = document.querySelector("#category-show")
+
 class Category {
-    constructor(categoryJSON) {
-        this.id = categoryJSON.id
-        this.name = categoryJSON.name
+    static all = []
+    constructor({id, name}) {
+        this.id = id
+        this.name = name
+        Category.all.push(this)
     }
-    renderLi() {
-        return `<li>${this.name}</li>`
+
+    static fetchCategories(){
+        fetch(baseURL + "categories")
+        .then(res => res.json())
+        .then(categoryData => {
+            categoryData.forEach(category => {
+                let x = new Category(category)
+                x.addToDom()
+            })
+        })
     }
+
+    addToDom(){
+        // this.categoriesList.appendChild(this.setElementHTML())
+        let cn = document.createElement('h1');
+        cn.src = this.name
+        cn.id = `category-${this.id}`
+        // cn.categoryName = 'coffee-size'
+        categoriesList.appendChild(cn)
+        // img.addEventListener('click',(e) => this.handleClick(e))
+    }
+
+//     setElementHTML(){
+//         this.element.innerHTML = `
+//         <h1>${this.name}</h1>
+//         `   
+//         return this.element
+//    }
+    
+    // renderLi() {
+    //     return `<li>${this.name}</li>`
+    // }
 }
 
 
